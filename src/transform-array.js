9fed9bbe9f85359f9d9a7a61b68280150dc3bf98
WerJJJ <marikariwest@gmail.com>
2020-10-06 19:54:45 +0300

    feat: find solution hanoi-tower

--- src/transform-array.js
+++ src/transform-array.js
@@ -1,6 +1,45 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function transform(/* arr */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function transform(arr) {
+  let newArr = arr;
+   if (Array.isArray(arr)){
+    for (let i = 0; i < newArr.length; i++){
+      if ( newArr[i] == '--discard-next'){
+        if (arr[i+1] == undefined){
+          newArr.splice(i,1);
+        }else{
+          delete arr[i];
+          delete arr[i+1];
+        }
+      }else
+      if ( newArr[i] == '--discard-prev'){
+        if (arr[i-1] == undefined){
+          newArr.splice(i,1);
+        }else{
+          delete arr[i];
+          delete arr[i-1];
+        }
+      }else 
+      if ( newArr[i] == '--double-next'){
+        if (arr[i+1] == undefined){
+          newArr.splice(i,1);
+        }else{
+          newArr.splice(i,1,arr[i+1]);
+        }
+      }else
+      if ( newArr[i] == '--double-prev'){
+        if (arr[i-1] == undefined){
+          newArr.splice(i,1);
+        }else{
+          newArr.splice(i,1,arr[i-1]);
+        }
+      }
+    }
+    newArr = newArr.filter(function (el) {
+      return el != null;
+    });
+    return newArr;
+   }else{
+     throw new Error;
+   }
 };
